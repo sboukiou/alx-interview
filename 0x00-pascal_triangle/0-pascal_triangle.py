11b0fcbce0e4f8fbb37e8c147693e652b340e321
diasbouk <boukdsaid@gmail.com>
2024-10-06 15:42:05 +0100

    Using the prev row to count instead of factorials cause they get into overflow large number, ( 100 ) as example

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,23 +4,6 @@
 """
 
 
-def factorial(n):
-    """
-    Get the factorial of n
-    """
-    result = 1
-    for i in range(1, n + 1):
-        result *= i
-    return result
-
-
-def combination(n, r):
-    """
-    Get the combination of n/r
-    """
-    return (factorial(n)) / (factorial(r) * factorial(n - r))
-
-
 def pascal_triangle(n):
     """
     Get the triangle of n elements
@@ -29,8 +12,9 @@ def pascal_triangle(n):
         return []
     triangle = [[1]]
     for i in range(1, n):
-        line = []
-        for j in range(0, i + 1):
-            line.append(int(combination(i, j)))
+        line = [1]
+        for j in range(1, i):
+            line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        line.append(1)
         triangle.append(line)
     return triangle
